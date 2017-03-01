e224ff9983980201254505b4b9da863d88d9b74d
邱东岳 <928079375@qq.com>
2017-03-01 13:22:00 +0800

    3.1上午

--- js/teacher/add.js
+++ js/teacher/add.js
@@ -1,7 +1,20 @@
 define(['jquery','common','nprogress'], function($,undefined,nprogress) {
 
-
-
-
 	nprogress.done();
+
+	$("#teacher-add-form").on("submit", function () {
+		$.ajax({
+			url:"/v6/teacher/add",
+			type:"post",
+			data:$(this).serialize(),
+			success: function (data) {
+				if(data.code==200){
+					location.href="/html/teacher/list.html";
+				}
+			}
+		})
+		return false;
+	});
+
+
 });
--- js/teacher/list.js
+++ js/teacher/list.js
@@ -3,13 +3,29 @@ define(['jquery','common','nprogress','template'], function($,undefined,nprogres
 	nprogress.done();
 
 
-	//��ʦ�б�ģ������
-	$.get("/v6/teacher", function (data) {
-		if(data.code==200){
-			$("#teacher-list-tbody").html(template("teacher_list",{list:data.result}));
-		}
-	});
+	//讲师列表数据缓存
+	var teacher=null;
 
+	try{
+		teacher=JSON.parse(localStorage.getItem('teacher'));
+	}catch(e){}
+
+	//讲师列表请求
+	if(teacher){
+		$("#teacher-list-tbody").html(template("teacher_list",{list:teacher}));
+	}else{
+		$.get("/v6/teacher",function (data) {
+			if(data.code==200){
+
+				localStorage.setItem('teacher',JSON.stringify(data.result));
+				$("#teacher-list-tbody").html(template("teacher_list",{list:data.result}));
+			}
+		});
+	}
+
+
+
+	//讲师信息请求
 	$("#teacher-list-tbody").on("click",".teacher-view", function () {
 		$.get("/v6/teacher/view",{tc_id:$(this).parent().attr("data-id")}, function (data) {
 			if(data.code==200){
@@ -17,8 +33,26 @@ define(['jquery','common','nprogress','template'], function($,undefined,nprogres
 			}
 		})
 
-
 	})
 
+	//讲师状态请求
+	$("#teacher-list-tbody").on("click",".teacher-status", function () {
+		var _this=$(this);
+		$.ajax({
+			url:"/v6/teacher/handle",
+			type:"post",
+			data:{
+				tc_status:$(this).parent().attr("data-status"),
+				tc_id:$(this).parent().attr("data-id")
+			},
+			success: function (data) {
+				if(data.code==200){
+					_this.html(data.result.tc_status==0?"开启":"注销");
+					_this.parent().attr("data-status",data.result.tc_status);
+				}
+			}
+		})
+
+	})
 });
 
